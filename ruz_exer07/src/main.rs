ca945422e048faa84f5db350638488bc5905726b
gordian-eggo <columbus.knot@gmail.com>
2018-10-05 18:02:49 +0800

    Fixed get_choice(), 'Choice: ' is now on same line as input taker

--- ruz_exer07/src/main.rs
+++ ruz_exer07/src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::Write;
+
 struct Subject {
 	class_section: String,
 	course_code: String
@@ -9,6 +12,28 @@ struct Student {
 	subjects: Vec<Subject>
 }
 
+fn print_menu() {
+	println!("\n======MENU======");
+	println!("[1] Add student");
+	println!("[2] View all students");
+	println!("[3] Add subject to student");
+	println!("[4] Delete student");
+	println!("[5] Exit");
+	print!("\nChoice: ");
+
+}
+
+fn get_choice() -> String {
+	let mut temp = String::new();
+	io::stdout().flush().unwrap();
+	io::stdin().read_line(&mut temp).expect("Error");
+	temp.pop();
+	temp.to_string();
+	temp
+}
+
 fn main() {
-	println!("Initial commit!");
+	print_menu();
+	let mut choice = get_choice().parse::<u32>().unwrap();
+	println!("{}\n", choice);
 }
